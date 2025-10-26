65c3d7a6ba5e6ab79d2f78559c85a461bd1e1794
NightFury-9b71 <140273645+NightFury-9b71@users.noreply.github.com>
2025-10-26 14:51:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ backend/app/api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+from app.api.routers import contacts, login, phones, security_qas, users, utils
+
+app = FastAPI(
+    title="Contact On Demand API",
+    description="API for Contact On Demand application",
+    version="1.0.0",
+)
+
+
+@app.get("/greet")
+async def greet():
+    return {"message": "Hello World!"}
+
+
+app.include_router(contacts.router)
+app.include_router(login.router)
+app.include_router(phones.router)
+app.include_router(security_qas.router)
+app.include_router(users.router)
+app.include_router(utils.router)
